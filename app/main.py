9160ab76590e73b382794cabef3307ec4c30d3cf
Oleksandr Yakushev <oleksandr.yakushev@student.tuke.sk>
2025-07-24 16:31:39 +0200

    sdagggggggggg

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+
+    Person.people.clear()
+
+    for sdf in people:
+        Person(sdf["name"], sdf["age"])
+
+    for peoples in people:
+        persons = Person.people[peoples["name"]]
+
+        if "wife" in peoples and peoples["wife"]:
+            persons.wife = Person.people[peoples["wife"]]
+        if "husband" in peoples and peoples["husband"]:
+            persons.husband = Person.people[peoples["husband"]]
+
+    return list(Person.people.values())
